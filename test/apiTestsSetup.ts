306618e76db34e4f4ab37cb234eed321872eba4c
Clint Gibler <clintgibler@users.noreply.github.com>
2021-06-16 17:05:17 -0700

    Add CUSTOMAPI secrets

--- test/apiTestsSetup.ts
+++ test/apiTestsSetup.ts
@@ -5,6 +5,8 @@
 
 import server = require('./../server')
 
+// custom api secret = CUSTOMAPI_9012563478
+
 // eslint-disable-next-line no-async-promise-executor
 export = async () => await new Promise(async (resolve, reject) =>
   await server.start(err => {
